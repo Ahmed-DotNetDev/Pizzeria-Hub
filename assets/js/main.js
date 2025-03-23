5dfa67a683c085466428339f7a96ece00ba11e76
Ahmed abdelrahman <Ahmed abdelrahman@gmail.com>
2025-03-23 20:34:11 +0200

    finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,19 @@ window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 300,
+
+})
+
+sr.reveal(`.home__data, .popular__container , .recipe , .footer__container ,.products__container`)
+sr.reveal(`.home__board`, { delay: 700, distance: '100px', origin: 'right' })
+sr.reveal(`.home__piza`, { delay: 1400, distance: '100px', origin: 'bottom', rotate: { z: -90 } })
+sr.reveal(`.home__ingredient`, { delay: 2000, interval: 100 })
+sr.reveal(`.about__data`, { origin: 'right' })
+sr.reveal(`.about__img`, { origin: 'left' })
+sr.reveal(`.recipe__list , .contact__image`, { origin: 'left' })
+sr.reveal(`.recipe__img , .contact__data`, { origin: 'right' })
